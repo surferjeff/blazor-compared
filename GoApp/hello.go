222fbb9cf03a02d5fec33e21d2e2e9fccc430193
Jeffrey Rennie <jeff@rennieglen.com>
2023-09-23 13:58:31 -0700

    Copied templates.  HTTP Response is empty.

--- GoApp/hello.go
+++ GoApp/hello.go
@@ -1,14 +1,24 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"text/template"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
 	app.Static("/", "./wwwroot")
 
+	tmpl := template.Must(template.ParseFiles(
+		"templates/Index.html",
+		"templates/NavMenu.html",
+		"templates/MainLayout.html",
+		"templates/_Layout.html"))
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return tmpl.Execute(c, "layout")
 	})
 
 	app.Listen(":3000")
