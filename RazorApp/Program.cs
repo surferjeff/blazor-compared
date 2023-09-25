f3f732389e1be188b1f0d0d7ed0d4c52f8d70ad9
Jeffrey Rennie <jeff@rennieglen.com>
2023-09-25 11:21:58 -0700

    RazorApp nearing completion.

--- RazorApp/Program.cs
+++ RazorApp/Program.cs
@@ -1,7 +1,10 @@
+using BlazorApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
