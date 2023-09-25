f248d2e3cf2b9b3807c0f070c5e79b5fc1c6a45a
Jeffrey Rennie <jeff@rennieglen.com>
2023-09-25 09:23:27 -0700

    counter works

---
+++ RazorApp/Pages/Counter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class CounterModel : PageModel {
+    [BindProperty(SupportsGet = true)]
+    public int Count { get; set; } = 0;
+    
+    public IActionResult OnGet() => Page();
+
+    public IActionResult OnPost() => Page();
+
+}
