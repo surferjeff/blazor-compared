7f1e81d4769ae29b915afbb417329f623bafcabe
Jeffrey Rennie <jeff@rennieglen.com>
2023-09-25 10:16:55 -0700

    Counter works with htmx.

--- RazorApp/Pages/Counter.cshtml.cs
+++ RazorApp/Pages/Counter.cshtml.cs
@@ -4,6 +4,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 public class CounterModel : PageModel {
     [BindProperty(SupportsGet = true)]
     public int Count { get; set; } = 0;
-    
-    public IActionResult OnGet() => Page();
+    public string Layout { get; set; } = "_Layout";
+    public string Path { get; set; } = "";    
+
+    public IActionResult OnGet() {
+        Path = HttpContext.Request.Path;
+        if (HttpContext.Request.Headers["HX-Request"].Contains("true")) {
+            Layout = "_LayoutBodyOnly";
+        }
+        return Page();
+    }
 }
