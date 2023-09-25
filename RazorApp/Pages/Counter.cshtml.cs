ed7f3c7e3d9be6cbe39c04c4b8b962ecbd21d959
Jeffrey Rennie <jeff@rennieglen.com>
2023-09-25 09:30:11 -0700

    Make note of an inscrutable error.

--- RazorApp/Pages/Counter.cshtml.cs
+++ RazorApp/Pages/Counter.cshtml.cs
@@ -7,6 +7,10 @@ public class CounterModel : PageModel {
     
     public IActionResult OnGet() => Page();
 
+    // Because this handler never receives a post request, I thought the
+    // next line of code would be unnecessary.  But if I remove it,
+    // I see the error 
+    //   InvalidOperationException: Unsupported handler method return type 'ViewComponents.NavMenu'.
+    // with an inscrutable stack trace.    
     public IActionResult OnPost() => Page();
-
 }
