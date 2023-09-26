754db63e9311661a6752d01a2279c4ed61786c36
Jeffrey Rennie <surferjeff@gmail.com>
2023-09-26 16:24:09 -0700

    _LayoutBoosted

--- RazorApp/Pages/Counter.cshtml.cs
+++ RazorApp/Pages/Counter.cshtml.cs
@@ -4,17 +4,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 public class CounterModel : PageModel {
     [BindProperty(SupportsGet = true)]
     public int Count { get; set; } = 0;
-    public string Layout { get; set; } = "_Layout";
     public string Path { get; set; } = "";    
 
     public IActionResult OnGet() {
         Path = HttpContext.Request.Path;
-        var headers = HttpContext.Request.Headers;
-        if (headers["HX-Request"].Contains("true")
-            && !headers["HX-Boosted"].Contains("true"))
-        {
-            Layout = "_LayoutBodyOnly";
-        }
         return Page();
     }
 }
