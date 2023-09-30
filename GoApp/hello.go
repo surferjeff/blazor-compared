5538a2be5490b36d3752d9c5a82f68699c97490d
Jeffrey Rennie <jeff@rennieglen.com>
2023-09-30 09:39:41 -0700

    Use c.Vary() and RenderPage().

--- GoApp/hello.go
+++ GoApp/hello.go
@@ -10,17 +10,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func wrapWithLayout(c *fiber.Ctx, title string,
-	component templ.Component) templ.Component {
-	main := mainLayout(navMenu(c.Route().Path), component)
-	headers := c.GetReqHeaders()
-	if headers["Hx-Boosted"] == "true" {
-		return boostedLayout(title, main)
-	} else {
-		return layout(title, main)
-	}
-}
-
 type TemplViews struct {
 }
 
@@ -39,6 +28,18 @@ func RenderC(c *fiber.Ctx, component templ.Component) error {
 	return c.Render("", component)
 }
 
+// Wraps with Layout.
+func RenderPage(c *fiber.Ctx, title string,
+	component templ.Component) error {
+	main := mainLayout(navMenu(c.Route().Path), component)
+	headers := c.GetReqHeaders()
+	layout := layout(title, main)
+	if headers["Hx-Boosted"] == "true" {
+		layout = boostedLayout(title, main)
+	}
+	return RenderC(c, layout)
+}
+
 func main() {
 	templViews := new(TemplViews)
 	app := fiber.New(fiber.Config{
@@ -48,24 +49,24 @@ func main() {
 	app.Static("/", "./wwwroot")
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		c.Set("Vary", "HX-Boosted")
-		return RenderC(c, wrapWithLayout(c, "Home", index()))
+		c.Vary("HX-Boosted")
+		return RenderPage(c, "Home", index())
 	})
 	app.Get("/about", func(c *fiber.Ctx) error {
-		c.Set("Vary", "HX-Boosted")
-		return RenderC(c, wrapWithLayout(c, "About", about()))
+		c.Vary("HX-Boosted")
+		return RenderPage(c, "About", about())
 	})
 	app.Get("/counter", func(c *fiber.Ctx) error {
-		c.Set("Vary", "HX-Boosted")
-		return RenderC(c, wrapWithLayout(c, "Counter", counter(0)))
+		c.Vary("HX-Boosted")
+		return RenderPage(c, "Counter", counter(0))
 	})
 	app.Get("/increment", func(c *fiber.Ctx) error {
 		count := c.QueryInt("count", 0)
 		return RenderC(c, counter(count))
 	})
 	app.Get("/fetchdata", func(c *fiber.Ctx) error {
-		c.Set("Vary", "HX-Boosted")
-		return RenderC(c, wrapWithLayout(c, "Weather forecast", fetchData()))
+		c.Vary("HX-Boosted")
+		return RenderPage(c, "Weather forecast", fetchData())
 	})
 	app.Post("/forecasts", func(c *fiber.Ctx) error {
 		return RenderC(c, forecasts(getForecasts(time.Now())))
