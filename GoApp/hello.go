1c7ac7eaaf6e9632e4f69604b3c96e64e12a73a5
Jeffrey Rennie <jeff@rennieglen.com>
2023-10-03 18:15:44 -0700

    Reduce lines of code.

--- GoApp/hello.go
+++ GoApp/hello.go
@@ -29,6 +29,7 @@ func RenderC(c *fiber.Ctx, component templ.Component) error {
 // Wraps with Layout.
 func RenderPage(c *fiber.Ctx, title string,
 	component templ.Component) error {
+	c.Vary("HX-Boosted")
 	main := mainLayout(navMenu(c.Route().Path), component)
 	headers := c.GetReqHeaders()
 	layout := layout(title, main)
@@ -44,15 +45,12 @@ func main() {
 	app.Static("/", "./wwwroot")
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		c.Vary("HX-Boosted")
 		return RenderPage(c, "Home", index())
 	})
 	app.Get("/about", func(c *fiber.Ctx) error {
-		c.Vary("HX-Boosted")
 		return RenderPage(c, "About", about())
 	})
 	app.Get("/counter", func(c *fiber.Ctx) error {
-		c.Vary("HX-Boosted")
 		return RenderPage(c, "Counter", counter(0))
 	})
 	app.Get("/increment", func(c *fiber.Ctx) error {
@@ -60,7 +58,6 @@ func main() {
 		return RenderC(c, counter(count))
 	})
 	app.Get("/fetchdata", func(c *fiber.Ctx) error {
-		c.Vary("HX-Boosted")
 		return RenderPage(c, "Weather forecast", fetchData())
 	})
 	app.Post("/forecasts", func(c *fiber.Ctx) error {
