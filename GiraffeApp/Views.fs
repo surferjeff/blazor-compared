07d13de818a7c0ab252c913bc64bd173110f5f23
Jeffrey Rennie <jeff@rennieglen.com>
2023-10-21 15:27:28 -0700

    mainLayout

--- GiraffeApp/Views.fs
+++ GiraffeApp/Views.fs
@@ -25,14 +25,35 @@ let layout (atitle: string) (content: XmlNode list) =
         ]
     ]
 
+// HTMX Attributes
 let _hx_swap_oob (targetCssSelector: string) = 
     KeyValue("hx-swap-oob", targetCssSelector)
+
+let _hx_target (targetCssSelector: string) = 
+    KeyValue("hx-target", targetCssSelector)
+
+let _hx_boost (boost: bool) = 
+    KeyValue("hx-swap-oob", boost.ToString())
 
 let boostedLayout (atitle: string) (content: XmlNode list) =
     title [ _hx_swap_oob "title" ] [ encodedText atitle ] :: content
 
+let mainLayout (navMenu: XmlNode list) (mainArticle: XmlNode list) = [
+    div [ _class "page" ] [
+        div [ _class "sidebar" ] navMenu
+        main [] [
+            div [ _class "top-row px-4" ] [
+                a [ _href "/about"
+                    _hx_boost true
+                    _hx_target "#main-layout" ] [ encodedText "About" ] ]
+            article [ _class "content px-4 article"
+                      _id "main-article" ] mainArticle ] ] ]
+
+
+/////////////////////////////////////////////////////////////////////////
 let partial () =
     h1 [] [ encodedText "GiraffeApp" ]
+
 
 let index (message : string) =
     [
