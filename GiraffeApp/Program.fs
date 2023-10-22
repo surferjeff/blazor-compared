4c2883b0572e41d14317f0cecb6ead98eacf92d7
Jeffrey Rennie <jeff@rennieglen.com>
2023-10-22 14:08:25 -0700

    htmlNodes fixes the runtime issue.

--- GiraffeApp/Program.fs
+++ GiraffeApp/Program.fs
@@ -17,39 +17,25 @@ open Giraffe.ViewEngine
 // Web app
 // ---------------------------------
 
-let rec writeBytesAsync (ctx: HttpContext option)(bytesList: byte[] list) =
-    task {
-        match (ctx, bytesList) with
-        | (Some(x), head :: tail) ->
-            let! nextCtx = x.WriteBytesAsync head
-            return! writeBytesAsync nextCtx tail
-        | (finalCtx, _) -> return finalCtx
-    }
-
-/// <summary>
-/// <para>Compiles a `Giraffe.GiraffeViewEngine.XmlNode` object to a HTML view and writes the output to the body of the HTTP response.</para>
-/// <para>It also sets the HTTP header `Content-Type` to `text/html` and sets the `Content-Length` header accordingly.</para>
-/// </summary>
-/// <param name="htmlView">An `XmlNode` object to be send back to the client and which represents a valid HTML view.</param>
-/// <returns>A Giraffe `HttpHandler` function which can be composed into a bigger web application.</returns>
-let htmlViews (htmlNodes : XmlNode list)(_ : HttpFunc) (ctx: HttpContext) : HttpFuncResult =
-    let bytesList = List.map RenderView.AsBytes.htmlDocument htmlNodes
-    ctx.SetContentType "text/html; charset=utf-8"
-    writeBytesAsync (Some ctx) bytesList
+let htmlNodes (htmlNodes : XmlNode list) : HttpHandler =
+    let bytes = RenderView.AsBytes.htmlNodes htmlNodes
+    fun (_ : HttpFunc) (ctx : HttpContext) ->
+        ctx.SetContentType "text/html; charset=utf-8"
+        ctx.WriteBytesAsync bytes
 
 let indexHandler (next: HttpFunc)(ctx: HttpContext): HttpFuncResult =
     let boosted = match ctx.Request.Headers.HxBoosted with
                     | Some(b) -> b
                     | None -> false
     let view =  Views.index ctx.Request.Path.Value boosted
-    htmlViews view next ctx
+    htmlNodes view next ctx
 
 let webApp =
     choose [
         GET >=>
             choose [
                 route "/" >=> indexHandler
-                route "/about" >=> htmlViews Views.about
+                route "/about" >=> htmlNodes Views.about
             ]
         setStatusCode 404 >=> text "Not Found" ]
 
