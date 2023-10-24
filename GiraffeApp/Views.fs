1897d869fd44a098cf49fbcd5c73bbcea8a3d674
Jeffrey Rennie <jeff@rennieglen.com>
2023-10-23 20:38:47 -0700

    Terser.

--- GiraffeApp/Views.fs
+++ GiraffeApp/Views.fs
@@ -18,21 +18,15 @@ let layout (atitle: string) (content: XmlNode list) = [
             meta [ _name "viewport"
                    _content "width=device-width, initial-scale=1.0" ]
             title []  [ encodedText atitle ]
-            link [ _rel  "stylesheet"
-                   _type "text/css"
+            link [ _rel  "stylesheet"; _type "text/css";
                    _href "/css/bootstrap/bootstrap.min.css" ]
-            link [ _rel  "stylesheet"
-                   _type "text/css"
+            link [ _rel  "stylesheet"; _type "text/css";
                    _href "/css/open-iconic/font/css/open-iconic-bootstrap.min.css" ]                   
-            link [ _rel  "stylesheet"
-                   _type "text/css"
-                   _href "/css/BlazorApp.styles.css" ]
-        ]
+            link [ _rel  "stylesheet"; _type "text/css";
+                   _href "/css/BlazorApp.styles.css" ] ]
         body [] [
             div [ _id "main-layout"] content
-            script [ _src "/htmx1.9.6.min.js" ] []
-        ]
-    ] ]
+            script [ _src "/htmx1.9.6.min.js" ] [] ] ] ]
 
 let boostedLayout (atitle: string) (content: XmlNode list) =
     title [ _hx_swap_oob "title" ] [ encodedText atitle ] :: content
