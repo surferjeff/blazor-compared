847fff14ebe45f5ae673618173d19edd1634f5ca
Jeffrey Rennie <jeff@rennieglen.com>
2023-10-25 21:54:32 -0700

    Script to gzip source code.
    To compare total source code complexity.

---
+++ gzip-src.sh
@@ -0,0 +1,13 @@
+gzipDotNet() {
+    tar -czvf $1-src.tar.gz $(
+        find $1 -name "*.cs" -o -name "*.cshtml" -o -name "*.razor" -o -name "*.fs" \
+        | grep -vE "/(obj|bin)/" )
+}
+
+gzipDotNet BlazorApp
+gzipDotNet RazorApp
+gzipDotNet GiraffeApp
+
+tar -czvf GoApp-src.tar.gz GoApp/*.go GoApp/templates/*
+tar -czvf GoTemplApp-src.tar.gz $(
+    find GoTemplApp -name "*.go" -o -name "*.templ" | grep -v _templ.go)
