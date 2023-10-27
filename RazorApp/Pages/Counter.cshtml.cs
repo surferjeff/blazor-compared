5d127339ea39ef3bfd70dd9c3b8b20b5484c6c0d
Jeffrey Rennie <jeff@rennieglen.com>
2023-10-27 12:34:50 -0700

    Path need not live in CounterModel.

--- RazorApp/Pages/Counter.cshtml.cs
+++ RazorApp/Pages/Counter.cshtml.cs
@@ -4,10 +4,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 public class CounterModel : PageModel {
     [BindProperty(SupportsGet = true)]
     public int Count { get; set; } = 0;
-    public string Path { get; set; } = "";    
 
     public IActionResult OnGet() {
-        Path = HttpContext.Request.Path;
         return Page();
     }
 }
