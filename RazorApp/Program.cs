2e48fe90d573267e9491d42b65b68ef6ac0ad8af
Jeffrey Rennie <jeff@rennieglen.com>
2024-06-17 09:26:55 -0700

    /restart

--- RazorApp/Program.cs
+++ RazorApp/Program.cs
@@ -1,33 +1,45 @@
 using BlazorApp.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddSingleton<WeatherForecastService>();
-
-if ((Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "")
-    .ToLower() != "development")
+class Program
 {
-    var port = Environment.GetEnvironmentVariable("PORT");
-    if (port != null) {
-        builder.WebHost.UseUrls($"http://*:{port}");
+
+    static void Main(string[] args) {
+        while (true) {
+            _Main(args);
+        }
+    }
+
+    static void _Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddSingleton<WeatherForecastService>();
+
+        if ((Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "")
+            .ToLower() != "development")
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+            if (port != null) {
+                builder.WebHost.UseUrls($"http://*:{port}");
+            }
+        }
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        app.Run();
     }
 }
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
