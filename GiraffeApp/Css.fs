6e8bf61b4724d1952e26bf51b5971a0901183c78
Jeffrey Rennie <surferjeff@gmail.com>
2024-12-25 19:34:17 -0800

    this.toXmlNode

--- GiraffeApp/Css.fs
+++ GiraffeApp/Css.fs
@@ -2,7 +2,7 @@ module Css
 
 open System
 open System.Collections.Generic
-
+open Giraffe.ViewEngine
 
 [<Struct>]
 type ClassDef = {
@@ -29,16 +29,26 @@ let randomString (length: int) =
     String.init length (fun _ -> string chars[random.Next(chars.Length)])
 
 type Head() =
-    let classes = SortedDictionary<ClassDef, string>()
+    let classDefs = SortedDictionary<ClassDef, string>()
 
     member this.Add (classDef: ClassDef) =
-        match (classes.TryGetValue classDef), classDef.WithRandom with
+        match (classDefs.TryGetValue classDef), classDef.WithRandom with
         | (true, value), _  -> value
-        | _, false -> classes.Add(classDef, classDef.Name); classDef.Name
+        | _, false -> classDefs.Add(classDef, classDef.Name); classDef.Name
         | _, true ->
             let sep = if classDef.Name = "" then "" else "-"
             let rando = randomString 6
             let name = $"{classDef.Name}{sep}{rando}"
-            classes.Add(classDef, name)
+            classDefs.Add(classDef, name)
             name
-
+    
+    member this.toXmlNode() =
+        let mutable lines = []
+        for classDef in classDefs do
+            lines <- "}" :: lines
+            let classDef, className = classDef.Key, classDef.Value                
+            for decl in classDef.Decls do
+                lines <- $"\t{fst decl}: {snd decl};" :: lines
+            lines <- $"{className} {{" :: lines
+        let text = "" :: lines |> List.rev |> String.concat "\n"
+        style [] [str text]
