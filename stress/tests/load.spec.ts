4dba00f85315a51ee68a24651148945108692775
Jeffrey Rennie <jeff@rennieglen.com>
2025-05-25 18:56:51 -0700

    On Linux, BlazerApp's memory usage is bounded.
    It peaks around 240 MB, then runs steady around 140.

--- stress/tests/load.spec.ts
+++ stress/tests/load.spec.ts
@@ -4,7 +4,7 @@ for (let i = 0; i < 10000; i++) {
   const n = String(i + 1);
   const id = n.padStart(3, ' ');
   test(`load homepage - instance ${id}`, async ({ page }) => {
-    await page.goto('http://localhost:5085');
+    await page.goto('http://localhost:5085', {'waitUntil': 'networkidle' });
     await page.close();
   });
 };
