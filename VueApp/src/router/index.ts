155d7b3af39a2c755ece82fe0eb833ca5e59c9c4
Jeffrey Rennie <surferjeff@gmail.com>
2025-06-11 11:55:34 -0700

    CounterView, but page title isn't updating.

--- VueApp/src/router/index.ts
+++ VueApp/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CounterView from "../views/CounterView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
       meta: { title: 'Index' }
+    },
+    {
+      path: '/counter',
+      name: 'counter',
+      component: CounterView,
+      meta: { title: 'Counter' }
     },
     {
       path: '/about',
