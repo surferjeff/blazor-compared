a5e752d5e71798d36fe16ce3a330dde5669507a5
Jeffrey Rennie <surferjeff@gmail.com>
2025-06-11 10:23:13 -0700

    Copying from BlazorApp.

--- VueApp/src/views/HomeView.vue
+++ VueApp/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello, world!</h1>
+
+  Welcome to your new app.
+
+  <SurveyPrompt Title="How is Blazor working for you?" />
 </template>
+
+<script setup lang="ts">
+import SurveyPrompt from "@/components/SurveyPrompt.vue";
+</script>
