0d80851c8b9f0e9bb4bcfcc957df9ba2503b5de6
Jeffrey Rennie <surferjeff@gmail.com>
2025-07-14 16:19:58 -0700

    Proxy works but no static files are served.

--- MVCAppWithTypeScript/Program.cs
+++ MVCAppWithTypeScript/Program.cs
@@ -36,12 +36,36 @@ else
     app.UseHsts();
 }
 
-app.UseStaticFiles();
-
 app.UseRouting();
 
-// Map YARP reverse proxy BEFORE your MVC endpoints
-// app.MapReverseProxy();
+if (app.Environment.IsDevelopment()) {
+    // There's no way to call app.UseStaticFiles() and then have the proxy
+    // take priority over paths that could be served by both.
+    // Therefore, we have to implement a light-weight UseStaticFiles() here.
+    #pragma warning disable ASP0014 // Suggest using top level route registrations
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapReverseProxy();
+
+        endpoints.MapFallback(async context =>
+        {
+            var env = app.Services.GetRequiredService<IWebHostEnvironment>();
+            var fileProvider = env.WebRootFileProvider;
+            var fileInfo = fileProvider.GetFileInfo(context.Request.Path);
+
+            if (fileInfo.Exists)
+            {
+                await context.Response.SendFileAsync(fileInfo);
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+        });
+    });
+} else {
+    app.UseStaticFiles();
+}
 
 app.MapControllerRoute(
     name: "default",
