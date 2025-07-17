ce55699bb7c2aadfe30fb45f91c011690fe93dbd
Jeffrey Rennie <jeff@rennieglen.com>
2025-07-16 19:31:35 -0700

    On Linux, the watched files are constantly changing.  It's as if esbuild is touching them.

--- MVCAppWithTypeScript/scripts/Counter.ts
+++ MVCAppWithTypeScript/scripts/Counter.ts
@@ -1,6 +1,5 @@
 let count = 0;
 function onClick() {
-    document.getElementById("countSpan").innerText = String(count += 1);
+    document.getElementById("countSpan").innerText = String(count += 2);
 }
 document.getElementById("countBtn").onclick = onClick;
-// OK
